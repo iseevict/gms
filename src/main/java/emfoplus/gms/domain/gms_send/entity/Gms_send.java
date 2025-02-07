a5c75c7a71e9651e1160486e9f3735527be9626a
isee <stpark0509@gmail.com>
2025-02-07 16:16:42 +0900

    - Initial setting   - entity create   - need PK refactor   - Test File Delete

---
+++ src/main/java/emfoplus/gms/domain/gms_send/entity/Gms_send.java
@@ -0,0 +1,65 @@
+package emfoplus.gms.domain.gms_send.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "GMS_SEND")
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Gms_send {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long msgSeq;
+
+    @Column(nullable = false)
+    private String messageId;
+
+    private String networkId;
+
+    @Column(nullable = false, length = 20)
+    private String sender;
+
+    @Column(nullable = false, length = 40)
+    private String receiver;
+
+    @Column(nullable = false, length = 10)
+    private String countryCode;
+
+    @Column(nullable = false, length = 70)
+    private String text;
+
+    @Column(length = 100)
+    private String destination;
+
+    @Column(nullable = false)
+    private LocalDateTime sendAt;
+
+    private LocalDateTime doneAt;
+
+    private Integer messageCount;
+
+    private Double pricePerMessage;
+
+    @Column(length = 10)
+    private String currency;
+
+    @Column(length = 20)
+    private String rsltCode;
+
+    private String rsltStatusMemo;
+
+    private String rsltErrorMemo;
+
+    private Double emfoPrice;
+
+    private Double totalPrice;
+
+    @Column(length = 1)
+    private String status;
+}
